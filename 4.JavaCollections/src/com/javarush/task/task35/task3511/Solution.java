cfbbcfb0ad4b0ab69846ec76996419a9693ebbc4
VaSeBa <badasin.valentin@yandex.ru>
2020-10-11 14:53:25 +0300

    init

---
+++ 4.JavaCollections/src/com/javarush/task/task35/task3511/Solution.java
@@ -0,0 +1,52 @@
+package com.javarush.task.task35.task3511;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/* 
+sum - суммирует все элементы списка, в котором находятся любые числа,
+multiply - перемножает между собой все элементы списка, в котором находятся любые числа,
+concat - соединяет все элементы списка в одну строку,
+combine - принимает на вход список любых коллекций и возвращает результирующий список, в котором находятся все элементы из списков.
+*/
+public class Solution {
+
+    public static void main(String[] args) {
+    }
+
+    public static Double sum(List <? extends Number> list) {
+        Double result = 0.0;
+        for (int i = 0; i < list.size(); i++) {
+            Number numb = (Number) list.get(i);
+            result += numb.doubleValue();
+        }
+        return result;
+    }
+
+    public static Double multiply(List <? extends Number> list) {
+        Double result = 1.0;
+        for (int i = 0; i < list.size(); i++) {
+            Number numb = (Number) list.get(i);
+            result *= numb.doubleValue();
+        }
+        return result;
+    }
+
+    public static String concat(List <? extends Object> list) {
+        StringBuilder builder = new StringBuilder();
+        for (Object obj : list) {
+            builder.append(obj);
+        }
+        return builder.toString();
+    }
+
+    public static List combine(List <? extends Collection> list) {
+        List result = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            Collection collection = (Collection) list.get(i);
+            result.addAll(collection);
+        }
+        return result;
+    }
+}
